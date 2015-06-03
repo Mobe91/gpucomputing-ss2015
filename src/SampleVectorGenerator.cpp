a7bf0617d291ae0bf9f9c056e3ce30840b028a05
Moritz Becker <moritz.becker@gmx.at>
2015-06-03 09:15:15 +0200

    Implemented normalization of sample vectors

--- src/SampleVectorGenerator.cpp
+++ src/SampleVectorGenerator.cpp
@@ -35,6 +35,16 @@ void SampleVectorGenerator::generateSampleVectorsFromCIFAR(SampleVectorsHolder**
 	Mat descriptors;
 	VLADEncoder vladEncoder(VLAD_CENTERS, ORB_DESCRIPTOR_DIMENSION);
 
+	float min[ORB_DESCRIPTOR_DIMENSION];
+	float max[ORB_DESCRIPTOR_DIMENSION];
+
+	// init min/max arrays
+	for (int i = 0; i < ORB_DESCRIPTOR_DIMENSION; i++)
+	{
+		min[i] = std::numeric_limits<float>::max();
+		max[i] = std::numeric_limits<float>::min();
+	}
+
 	for (auto &loader : loaders)
 	{
 		do {
@@ -48,12 +58,39 @@ void SampleVectorGenerator::generateSampleVectorsFromCIFAR(SampleVectorsHolder**
 
 				if (descriptors.rows >= VLAD_CENTERS)
 				{
-					vladEncoder.encode(sampleVectors + sampleVectorsCount * VLAD_CENTERS * ORB_DESCRIPTOR_DIMENSION, descriptors);
+					float* currentSampleVector = sampleVectors  + sampleVectorsCount * VLAD_CENTERS * ORB_DESCRIPTOR_DIMENSION;
+					vladEncoder.encode(currentSampleVector, descriptors);
 					sampleVectorsCount++;
+
+					// update min max
+					for (int row = 0; row < VLAD_CENTERS; row++)
+					{
+						for (int col = 0; col < ORB_DESCRIPTOR_DIMENSION; col++)
+						{
+							min[col] = MIN(min[col], *(currentSampleVector + row * descriptors.cols + col));
+							max[col] = MAX(max[col], *(currentSampleVector + row * descriptors.cols + col));
+						}
+					}
+
 				}
 
 			}
 		} while (imgPair.second != -1);
+	}
+
+	// normalize values
+	for (int sampleVectorIdx = 0; sampleVectorIdx < sampleVectorsCount; sampleVectorIdx++)
+	{
+		float* currentSampleVector = sampleVectors + sampleVectorIdx * VLAD_CENTERS * ORB_DESCRIPTOR_DIMENSION;
+		for (int row = 0; row < VLAD_CENTERS; row++)
+		{
+			for (int col = 0; col < ORB_DESCRIPTOR_DIMENSION; col++)
+			{
+				float* currentVectorElement = currentSampleVector + row * ORB_DESCRIPTOR_DIMENSION + col;
+				*currentVectorElement = (*currentVectorElement - min[col]) / (max[col] - min[col]);
+				assert(*currentVectorElement >= 0.0f && *currentVectorElement <= 1.0f);
+			}
+		}
 	}
 
 	*out = new SampleVectorsHolder(sampleVectors, sampleVectorsCount, VLAD_CENTERS, ORB_DESCRIPTOR_DIMENSION);
